435246b0eb8774ca9bc638be4175a488a73b5ac9
aewinformatica <aewinformatica@gmail.com>
2020-02-20 17:41:03 -0300

    Inclusão Spring,Conectores de BD, Configuração da Classe Application

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Sistema de Controle de Estoque e Vendas - Desktop
 
 * Criação da Estrutura do Projeto usando Maven;
+* Inclusão Spring,Conectores de BD, Configuração da Classe Application
--- src/main/java/com/aewinformatica/scev/desktop/Application.java
+++ src/main/java/com/aewinformatica/scev/desktop/Application.java
@@ -1,12 +1,24 @@
 package com.aewinformatica.scev.desktop;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
 /**
  *
  * @author Jessica
  */
+@SpringBootApplication
 public class Application {
     
     public static void main(String[] args) {
         
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class)
+                .headless(false)
+                .web(WebApplicationType.NONE);
+
+        ConfigurableApplicationContext context;
+        context = builder.run(args);
     }
 }
