5d33ccba83d9ee6c2cb4bdf33f91a24585c8a92b
aewinformatica <aewinformatica@gmail.com>
2020-02-20 17:32:30 -0300

    Criação da Estrutura do Projeto Usando Maven

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SCEV-Desktop
 Sistema de Controle de Estoque e Vendas - Desktop
+
+* Criação da Estrutura do Projeto usando Maven;
---
+++ src/main/java/com/aewinformatica/scev/desktop/Application.java
@@ -0,0 +1,12 @@
+package com.aewinformatica.scev.desktop;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Application {
+    
+    public static void main(String[] args) {
+        
+    }
+}
